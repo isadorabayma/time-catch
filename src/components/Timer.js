fc8ca1717dc63971e97b31cf865d33e2121e4d8a
isadora <isadora.bayma@gmail.com>
2022-11-26 20:08:41 -0300

    feat: Timer componets add constructor state

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,15 @@
-import logo from '../assets/play.png';
+// import logo from '../assets/play.png';
 import React, { Component } from "react";
 
 class Timer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            seconds: 0,
+            minutes: 0,
+        }
+    }
+
     render() {
         return(
             <div className="App-header">
@@ -9,7 +17,8 @@ class Timer extends Component {
                     . Timer  . | . Pomodoro . | . Counter .
                 </p>
                 <div className="App-timer">
-                    <img src={logo} className="App-play" alt="logo" />
+                    {/* <img src={logo} className="App-play" alt="logo" /> */}
+                    <h2 className="App-timer-number">0{this.state.seconds}:0{this.state.minutes}</h2>
                 </div>
                 <p>
                     Start
