be974c07231af71a44650403967e21bad11a0d07
isadora <isadora.bayma@gmail.com>
2022-11-26 21:03:21 -0300

    feat: BtnStart and BtnPause componets

---
+++ src/components/BtnPause.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BtnPause extends Component {
+    render() {
+        return(
+            <button className="App-btn">
+                Pause
+            </button>
+        );
+    };
+};
+
+export default BtnPause;
---
+++ src/components/BtnStart.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BtnStart extends Component {
+    render() {
+        return(
+            <button className="App-btn">
+                Start
+            </button>
+        );
+    };
+};
+
+export default BtnStart;
