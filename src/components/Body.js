9e4e70c5bc8bd2f9d84232ff374cfa69d8b9dd64
isadora <isadora.bayma@gmail.com>
2022-11-27 11:07:27 -0300

    feat: Body componet choose type

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,44 @@
 import React, {Component} from "react";
 import Button from "./Button";
 import Counter from "./Counter";
-// import Timer from "./Timer";
-// import Pomodoro from "./Pomodoro";
+import Timer from "./Timer";
+import Pomodoro from "./Pomodoro";
 
 class Body extends Component {
-  render() {
-    return(
-      <div className="App-header">
-          <div className="Align-Line">
-              <Button title="Timer" />
-              <Button title="Pomodoro"/>
-              <Button title="Counter"/>
-          </div>
-          {/* <Timer/> */}
-          <Counter/>
-          {/* <Pomodoro/> */}
-      </div>
-    )
-  }
+    constructor() {
+        super();
+        this.state = {
+            showTimer: true,
+            showPomodoro: false,
+            showCounter: false,
+        }
+
+        this.chooseType = this.chooseType.bind(this);
+    }
+
+    chooseType(type) {
+        this.setState(() => ({
+            showTimer: "Timer" === type ? true : false,
+            showPomodoro: "Pomodoro" === type ? true : false,
+            showCounter: "Counter" === type ? true : false,
+        }));
+    }
+
+    render() {
+        const { showTimer, showPomodoro, showCounter } = this.state;
+        return(
+        <div className="App-header">
+            <div className="Align-Line">
+                <Button onClick={() => {this.chooseType("Timer")}} title="Timer" />
+                <Button onClick={() => {this.chooseType("Pomodoro")}} title="Pomodoro"/>
+                <Button onClick={() => {this.chooseType("Counter")}} title="Counter"/>
+            </div>
+            {showTimer && <Timer/>}
+            {showPomodoro && <Pomodoro/>}
+            {showCounter && <Counter/>}
+        </div>
+        )
+    }
 }
 
 export default Body;
