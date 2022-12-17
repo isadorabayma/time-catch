23cf4de2ea7fad3415db2f960b1021f85f16e0f3
isadora <isadora.bayma@gmail.com>
2022-12-17 03:10:29 -0300

    Feat: getting started at save time data

---
+++ src/data/times.js
@@ -0,0 +1,16 @@
+const times = [
+  {
+    id: 1,
+    durationMin: 30,
+    startDataAndTime: "12:20 17/12/2022",
+    categoryId: 4,
+  },
+  {
+    id: 2,
+    durationMin: 78,
+    startDataAndTime: "14:20 17/12/2022",
+    categoryId: 2,
+  },
+]
+
+export default times;
